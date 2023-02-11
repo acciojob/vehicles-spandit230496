fc1f85c6f5161fd96fc90429b720f253ec1258a9
spandit230496 <sandip.pandit230496@gmail.com>
2023-02-11 14:28:29 +0530

    corrected

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -3,14 +3,13 @@ package com.driver;
 public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
-
-        this.name=name;
-        this.isManual=isManual;
+        super(name,4,4,6,isManual,"farrari",4);
+        //isManual=false;
         //Use arbitrary values for parameters which are not mentioned
     }
 
-    public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+    public void accelerate(int rate) {
+        int newSpeed = getCurrentSpeed() + rate; //set the value of new speed by using currentSpeed and rate
 
 //         * speed 0: gear 1
 //         * speed 1-50: gear 1
@@ -19,18 +18,34 @@ public class F1 extends Car {
 //         * speed 151-200: gear 4
 //         * speed 201-250: gear 5
 //         * speed more than 250: gear 6
-         */
 
-        if(newSpeed == 0) {
 
-            //Stop the car, set gear as 1
-            this.gears=1;
+        if (newSpeed == 0) {
+
+            stop();
+            changeGear(1);
+
+
         }
+
         //for all other cases, change the gear accordingly
 
-        if(newSpeed > 0) {
+        if(newSpeed > 0)
             changeSpeed(newSpeed, getCurrentDirection());
-                this.speed=newSpeed;
+
+        if(newSpeed>0&&newSpeed<=50)
+                changeGear(1);
+        else  if(newSpeed>=51&&newSpeed<=100)
+                changeGear(2);
+        else if(newSpeed>=101&&newSpeed<=150)
+                changeGear(3);
+        else if(newSpeed>=151&&newSpeed<=200)
+                changeGear(4);
+        else if(newSpeed>=201&&newSpeed<=250)
+                changeGear(5);
+        else if(newSpeed>=250)
+                changeGear(6);
+
         }
     }
-}
+
--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -46,8 +46,8 @@ public class Vehicle {
 
     public void move(int speed, int direction){
         //set the values of currentSpeed and currentDirection
-        direction=this.currentDirection;
-        speed=this.currentSpeed;
+        this.currentDirection=direction;
+        this.currentSpeed=speed;
         System.out.println("move method called - The speed is changed to: " + currentSpeed + ", and the direction is changed to: " + currentDirection + " degrees");
     }
 
